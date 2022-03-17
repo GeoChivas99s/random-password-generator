e465ede4539e022fc8a4b890890afd57ab753225
Geovane Silvio <Geovane.Silvio@frncubo.com>
2022-03-17 08:02:35 +0000

    feat: add styles on main content

--- src/components/main-content/index.jsx
+++ src/components/main-content/index.jsx
@@ -5,7 +5,11 @@ import styled from 'styled-components';
 const MainContentWrapper = styled.div`
 background:rgba(255, 255, 255, 0.25);
 box-shadow:0 8px 32px 0 rgba(31, 38 , 135, 0.37);
-
+backdrop-filter:blur(4px);
+-webkit-background-filter:blur(4px);
+border-radius:10px;
+border:1px solid rgba(255, 255, 255, 0.18);
+padding:100px 200px;
 
 
 `
